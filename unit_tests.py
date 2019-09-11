5d3747fe684074606fb058e3e616056bc0f1a6d4
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-11 16:24:39 -0400

    minor change to the unit testing structure and a comment or two;

--- unit_tests.py
+++ unit_tests.py
@@ -3,6 +3,9 @@ import unittest
 #Here we will define our test suites
 #test-cases are packaged into the testcases package
 
+no_tests = unittest.TestSuite() #an empty test suite is no tests
+
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
-    runner.run(unittest.TestSuite())
+
+    runner.run(no_tests)
