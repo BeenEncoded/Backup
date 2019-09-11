b36992b663806b02256ed3449f40f53acfd6c3f5
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-11 16:24:10 -0400

    renamed backup_main to main; made the iteration test more interesting;

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys, os
+from UI.MainWindow import display_gui
+from filesystem.iterators import CopyIterator
+
+if __name__ == "__main__":
+    print("\n\n\n\nSTARTING ITERATION: \n\n\n\n")
+    p = os.path.abspath("/home/jonathan")
+    count = 0
+    for entry in CopyIterator(p):
+        count += 1
+        if os.path.isfile(entry):
+            print("File: " + entry)
+        elif os.path.isdir(entry):
+            print("directory: " + entry)
+        else:
+            print("UNIDENTIFIED: " + entry)
+    print("\n\nDone iterating over " + str(count) + " paths under " + p)
+    #sys.exit(display_gui(sys.argv))
+    sys.exit(0)
