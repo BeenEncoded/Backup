78dfff3807187370fe4fb223cc47306b79b7296b
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-11 16:25:15 -0400

    wrote a recursive directory iterator; added launch config for vscode;

--- filesystem/iterators.py
+++ filesystem/iterators.py
@@ -1,11 +1,41 @@
 import os
 
-class CopyIterator:
+# This is basically just a wrapper class around os.walk, but it actually
+# iterates over everything.
+class RecursiveIterator:
     def __init__(self, root_path):
         self.iter = os.walk(root_path)
+        self.returned_parent = False
+        self.files = None
+        self.files_pos = 0
+        self.dirs = None
+        self.path = None
     
     def __iter__(self):
         return self
     
     def __next__(self):
-        return next(self.iter)[0]
+        if self.files is not None:
+            if self.files_pos >= len(self.files):
+                self.returned_parent = False
+        if self.returned_parent == False:
+            self.path, self.dirs, self.files = next(self.iter)
+            if self.files is not None:
+                self.returned_parent = True
+                self.files_pos = 0
+            return self.path
+        elif self.files is not None:
+            if self.files_pos < len(self.files):
+                s = os.path.join(self.path, self.files[self.files_pos])
+                self.files_pos += 1
+                return s
+
+class CopyIterator:
+    def __init__(self, root_path):
+        self.iter = RecursiveIterator(root_path)
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        return next(self.iter)
