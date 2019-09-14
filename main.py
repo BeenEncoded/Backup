c1a2b15c6deb18709d3c9171e7781e4f8981b746
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-14 00:26:53 -0400

    minor changes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys, os
 from UI.MainWindow import display_gui
-from filesystem.iterator import recursivecopy
+from filesystem.iterator import recursivecopy, recursive
 
 if __name__ == "__main__":
     print("\n\n\n\n\nSTARTING ITERATION: \n")
-    p = os.path.abspath("/home/jonathan")
+    p = os.path.abspath("../../..")
     count = 0
     for entry in recursivecopy(p):
         count += 1
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         else:
             print("UNIDENTIFIED: " + entry)
     print("\n\nDone iterating over " + str(count) + " paths under " + p)
-    #sys.exit(display_gui(sys.argv))
     sys.exit(0)
+    #sys.exit(display_gui(sys.argv))
