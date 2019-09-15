31e3e1cb1fd632dd7e07aa12c52122084e82bd94
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-15 00:00:50 -0400

    work on recursivecopy iterator and a  test to make sure it works;

--- filesystem/iterator.py
+++ filesystem/iterator.py
@@ -31,11 +31,39 @@ class recursive:
                 return s
 
 class recursivecopy:
-    def __init__(self, root_path):
+    def __init__(self, root_path, destination_folders):
+        if (root_path is None) or (destination_folders is None):
+            raise AttributeError("recursivecopy: invalid arguments")
+        if (os.path.isfolder(root_path) != True):
+            raise NotADirectoryError("recursivecopy: root_path argument not a folder")
+        if isinstance(destination_folders, list) != True:
+            raise AttributeError("recursivecopy: destination_folders is not an array!")
+        for path in destination_folders:
+            if os.path.isfolder(path) == False:
+                raise NotADirectoryError("recursivecopy: one of the destination folders passed to the \
+iterator is not a valid folder!")
+
         self.iter = recursive(root_path)
 
     def __iter__(self):
         return self
     
     def __next__(self):
-        return next(self.iter)
+        self.current = next(self.iter)
+
+# /c/abc
+# /c/abc/abc1/bac3
+# ['/c/abc', 'abc1/bac3']
+def split_path(parent, child):
+    if os.path.isabs(parent) == False:
+        parent = os.path.abspath(parent)
+    if os.path.isabs(child) == False:
+        child = os.path.abspath(child)
+    if ischild(parent, child):
+        return parent, child[(len(parent) + 1):]
+    return parent, ""
+
+def ischild(parent, child):
+    if len(parent) <= len(child):
+        return (parent == child[:len(parent)])
+    return False
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import sys, os
 from UI.MainWindow import display_gui
-from filesystem.iterator import recursivecopy, recursive
+from filesystem.iterator import recursive, split_path, ischild
 
 if __name__ == "__main__":
     print("\n\n\n\n\nSTARTING ITERATION: \n")
     p = os.path.abspath("../../..")
     count = 0
-    for entry in recursivecopy(p):
+    errors = list()
+    for entry in recursive(p):
         count += 1
-        if os.path.isfile(entry):
-            print("File: " + entry)
-        elif os.path.isdir(entry):
-            print("directory: " + entry)
-        else:
-            print("UNIDENTIFIED: " + entry)
+        print(split_path(p, entry))
+        if p != entry:
+            ic = ischild(p, os.path.join(split_path(p, entry)[0], split_path(p, entry)[1]))
+            print("second is child: " + str(ic))
+            if ic != True:
+                errors.append([entry, split_path(p, entry)])
+    if len(errors) > 0:
+        print("\n\n\n\nErrors: " + str(len(errors)))
+    else:
+        print("\n\n\n\nNo errors")
+    for e in errors:
+        print("Path: " + e[0])
+        print("split_path: " + e[1])
     print("\n\nDone iterating over " + str(count) + " paths under " + p)
     sys.exit(0)
     #sys.exit(display_gui(sys.argv))
