da6ebf283bfe77e80a4fb69d2fd5e0d693a21fb0
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-16 12:34:01 -0400

    main file now goes straight to gui

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from UI.MainWindow import display_gui
 from filesystem.iterator import recursive, recursivecopy, split_path, ischild
 
 if __name__ == "__main__":
-    
-    sys.exit(0)
-    #sys.exit(display_gui(sys.argv))
+    sys.exit(display_gui(sys.argv))
