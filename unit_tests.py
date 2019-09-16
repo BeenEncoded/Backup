b82d739159692f9be8a92e98350e6576bb5dd6fc
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-16 12:34:27 -0400

    changes testing targets;

--- unit_tests.py
+++ unit_tests.py
@@ -8,9 +8,11 @@ no_tests = unittest.TestSuite() #an empty test suite is no tests
 
 def defaultTests():
     s = unittest.TestSuite()
-    s.addTest(IterationTestCase('test_recursion'))
+    # s.addTest(IterationTestCase('test_recursion'))
     s.addTest(IterationTestCase('test_split_path'))
-    s.addTest(IterationTestCase('test_ischild'))
+    # s.addTest(IterationTestCase('test_ischild'))
+    # s.addTest(IterationTestCase('test_recursivecopy_initialization'))
+    s.addTest(IterationTestCase('test__copy_fsobject'))
     return s
 
 if __name__ == "__main__":
