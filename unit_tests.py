981932d6f71f70f92bdbe374652f9637231954c8
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-17 00:12:06 -0400

    minor change

--- unit_tests.py
+++ unit_tests.py
@@ -9,7 +9,7 @@ no_tests = unittest.TestSuite() #an empty test suite is no tests
 def defaultTests():
     s = unittest.TestSuite()
     # s.addTest(IterationTestCase('test_recursion'))
-    s.addTest(IterationTestCase('test_split_path'))
+    # s.addTest(IterationTestCase('test_split_path'))
     # s.addTest(IterationTestCase('test_ischild'))
     # s.addTest(IterationTestCase('test_recursivecopy_initialization'))
     s.addTest(IterationTestCase('test__copy_fsobject'))
@@ -17,5 +17,5 @@ def defaultTests():
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
-
     runner.run(defaultTests())
+    # unittest.main()
