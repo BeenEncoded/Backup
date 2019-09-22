a79a1af8c8ad6628a3a4d3ed60f686bab2e6e9c3
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-22 12:24:50 -0400

    rearranged how tests are added to the suite

--- unit_tests.py
+++ unit_tests.py
@@ -1,20 +1,28 @@
 import unittest
 from projecttests.filesystem import IterationTestCase
+from projecttests.data import DataTestCase
 
 #Here we will define our test suites
 #test-cases are packaged into the testcases package
 
 no_tests = unittest.TestSuite() #an empty test suite is no tests
 
+def addIterationTestCase(suite):
+    suite.addTest(IterationTestCase('test_recursion'))
+    suite.addTest(IterationTestCase('test_split_path'))
+    suite.addTest(IterationTestCase('test_ischild'))
+    suite.addTest(IterationTestCase('test_recursivecopy_initialization'))
+    # suite.addTest(IterationTestCase('test__copy_fsobject'))
+    # suite.addTest(IterationTestCase('test_backup_potential'))
+
+def addDataTestCase(suite):
+    suite.addTest(DataTestCase('test_jsonification'))
+
 def defaultTests():
-    s = unittest.TestSuite()
-    s.addTest(IterationTestCase('test_recursion'))
-    s.addTest(IterationTestCase('test_split_path'))
-    s.addTest(IterationTestCase('test_ischild'))
-    s.addTest(IterationTestCase('test_recursivecopy_initialization'))
-    # s.addTest(IterationTestCase('test__copy_fsobject'))
-    # s.addTest(IterationTestCase('test_backup_potential'))
-    return s
+    suite = unittest.TestSuite()
+    # addIterationTestCase(suite)
+    addDataTestCase(suite)
+    return suite
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
