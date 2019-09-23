0f0246c5f381e7f1b4c9e5527e5feca9b314fc98
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-23 13:54:25 -0400

    added better string representaions of various errors

--- errors.py
+++ errors.py
@@ -4,16 +4,25 @@ class CopyPathError(Exception):
     def __init__(self, message, errors = []):
         super(CopyPathError, self).__init__(message)
         self.errors = errors
+    
+    def __str__(self):
+        return "CopyPathError(\"" + self.message + "\"  errors: " + str(self.errors) + ")"
 
 class IncompleteCopyError(Exception):
     def __init__(self, message, errors):
         super(IncompleteCopyError, self).__init__(message)
         self.errors = errors
+    
+    def __str__(self):
+        return "IncompleteCopyError(\"" + self.message + "\"  errors: " + str(self.errors) + ")"
 
 class CopyFolderError(Exception):
     def __init__(self, message, errors):
         super(CopyFolderError, self).__init__(message)
         self.errors = errors
+    
+    def __str__(self):
+        return "CopyFolderError(\"" + self.message + "\"  errors: " + str(self.errors) + ")"
 
 class WrongArgumentTypeError(Exception):
     def __init__(self, message, errors):
