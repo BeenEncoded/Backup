2ce6745d5353fbd3c6a54ac428e56f759028df5e
Jonathan Whitlock <beenencoded@outlook.com>
2019-09-23 23:05:32 -0400

    work on the UI; added configuration to the UI;

--- UI/MainWindow.py
+++ UI/MainWindow.py
@@ -1,12 +1,24 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
 from UI.MainWindowWidgets import EditBackupProfileWidget
+from globaldata import *
 
 class MainWindow(QMainWindow):
     def __init__(self, parent):
         super(MainWindow, self).__init__(parent)
         self.setCentralWidget(EditBackupProfileWidget(self))
+        self._apply_configuration()
+    
+    def _apply_configuration(self):
+        '''
+        Applies the program's configuration to the UI.
+        '''
+        global CONFIG
+        uiconfig = CONFIG.getConfig()['ui']
+
+        self.setFont(QFont(str(uiconfig['font']), int(uiconfig['font_size'])))
 
 def display_gui(argv):
     app = QApplication(argv)
--- UI/MainWindowWidgets.py
+++ UI/MainWindowWidgets.py
@@ -11,9 +11,31 @@ class EditBackupProfileWidget(QWidget):
         self._init_layout()
     
     def _init_layout(self):
+        self.setWindowTitle("Edit Backup")
         mainlayout = QVBoxLayout()
-        label = QLabel("Hello World!")
-        label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
-        mainlayout.addWidget(label)
+        # A labeled text box:
+        temp_hbox = QHBoxLayout()
+        self.name_tbox = QLineEdit()
+        self.namelabel = QLabel("Backup Name:  ")
+        temp_hbox.addWidget(self.namelabel)
+        temp_hbox.addWidget(self.name_tbox)
+        mainlayout.addLayout(temp_hbox)
+
+        #A list box labeled with a groupbox for editing the list of source folders
+        self.sources_groupbox = QGroupBox("Source Folders:")
+        sources_gbox_layout = QVBoxLayout()
+        self.sources_listbox = QListWidget()
+        sources_gbox_layout.addWidget(self.sources_listbox)
+        self.sources_groupbox.setLayout(sources_gbox_layout)
+        mainlayout.addWidget(self.sources_groupbox)
+
+        #a list box labeled with a groupbox for editing the list of destination folders.
+        self.destinations_groupbox = QGroupBox("Destination Folders:")
+        dests_gbox_layout = QVBoxLayout()
+        self.destinations_listbox = QListWidget()
+        dests_gbox_layout.addWidget(self.destinations_listbox)
+        self.destinations_groupbox.setLayout(dests_gbox_layout)
+        mainlayout.addWidget(self.destinations_groupbox)
+
         self.setLayout(mainlayout)
