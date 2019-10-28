f7e07a348e6ef2ee14a7434d32dc79a58274360d
Jonathan Whitlock <beenencoded@outlook.com>
2019-10-28 19:20:24 -0400

    added version stamp

--- globaldata.py
+++ globaldata.py
@@ -13,9 +13,11 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import semver
 
 from data import Configuration, ProgramData
 
 #This file contains globals
+VERSION = semver.VersionInfo(1, 1, 0, "beta")
 CONFIG = Configuration()
 PDATA = ProgramData(_config=CONFIG.config)
