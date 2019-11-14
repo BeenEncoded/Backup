10b49a170583037e9158738d2ef67139cff0b30f
Jonathan Whitlock <beenencoded@outlook.com>
2019-11-13 21:02:24 -0500

    update build scripts for the src folder change;

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-python.exe -m PyInstaller --clean --win-private-assemblies --onefile -w -n backupwindows .\main.py
+python.exe -m PyInstaller --clean --win-private-assemblies --onefile -w -n backupwindows .\src\main.py
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-python3 -m PyInstaller --onefile -w -n backuplinux ./main.py
+python3 -m PyInstaller --onefile -w -n backuplinux ./src/main.py
