c95d991d77e9af903e05141dc08f72133e699227
Jonathan Whitlock <beenencoded@outlook.com>
2019-11-18 18:42:36 -0500

    formatted main

--- src/main.py
+++ src/main.py
@@ -14,21 +14,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import sys, atexit, os
+import sys
+import atexit
+import os
 from UI.MainWindow import display_gui
 from logging.handlers import RotatingFileHandler
 from globaldata import PDATA, LOGS_FOLDER, CONFIG, LOG_LEVEL, LOGFILE
 import logging
+
 
 def setup_logging():
     if not os.path.exists(LOGS_FOLDER):
         os.makedirs(LOGS_FOLDER)
 
     root = logging.getLogger()
-    f = logging.Formatter("%(asctime)s [%(name)s] [%(levelname)s] -> %(message)s")
+    f = logging.Formatter(
+        "%(asctime)s [%(name)s] [%(levelname)s] -> %(message)s")
     sh = logging.StreamHandler(sys.stdout)
     #fh = logging.FileHandler(LOGFILE)
-    fh = RotatingFileHandler(LOGFILE, mode='a', maxBytes=((2**20) * 2.5), backupCount=2, encoding=None, delay=False)
+    fh = RotatingFileHandler(
+        LOGFILE,
+        mode='a',
+        maxBytes=((2**20) * 2.5),
+        backupCount=2,
+        encoding=None,
+        delay=False)
 
     sh.setFormatter(f)
     fh.setFormatter(f)
@@ -38,6 +48,7 @@ def setup_logging():
     root.setLevel(LOG_LEVEL)
     root.info("log level: " + str(LOG_LEVEL))
 
+
 def onexit():
     global CONFIG
     global PDATA
@@ -45,18 +56,20 @@ def onexit():
     PDATA.save()
     logging.getLogger().info("[PROGRAM END]")
 
+
 if __name__ == "__main__":
     PDATA.load()
     setup_logging()
     logging.getLogger("main").info("[PROGRAM START]")
-    logging.getLogger("main").debug("Configuration: " + str(CONFIG.config._sections))
+    logging.getLogger("main").debug(
+        "Configuration: " + str(CONFIG.config._sections))
     logging.getLogger("main").debug("ProgramData: " + str(PDATA))
 
     atexit.register(onexit)
-    
+
     returnvalue = -1
     try:
         returnvalue = display_gui(sys.argv)
-    except: # noqa E722
+    except:  # noqa E722
         logging.getLogger().exception("CRITICAL EXCEPTION")
     sys.exit(returnvalue)
