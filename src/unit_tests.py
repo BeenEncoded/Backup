3a5dd44563b3321f9eb22302fd28911d3a338c10
Jonathan Whitlock <beenencoded@outlook.com>
2020-03-20 22:49:56 -0400

    restructured how tests are run;

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -15,32 +15,11 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
-from projecttests.filesystem import IterationTestCase
-from projecttests.data import DataTestCase
 
-#Here we will define our test suites
-#test-cases are packaged into the testcases package
-
-no_tests = unittest.TestSuite() #an empty test suite is no tests
-
-def addIterationTestCase(suite):
-    suite.addTest(IterationTestCase('test_recursion'))
-    suite.addTest(IterationTestCase('test_split_path'))
-    suite.addTest(IterationTestCase('test_ischild'))
-    suite.addTest(IterationTestCase('test_recursivecopy_initialization'))
-    # suite.addTest(IterationTestCase('test__copy_fsobject'))
-    # suite.addTest(IterationTestCase('test_backup_potential'))
-
-def addDataTestCase(suite):
-    suite.addTest(DataTestCase('test_jsonification'))
-
-def defaultTests():
-    suite = unittest.TestSuite()
-    addIterationTestCase(suite)
-    addDataTestCase(suite)
-    return suite
+#Test Cases:
+# from projecttests.filesystem import IterationTestCase
+# from projecttests.data import DataTestCase
+from projecttests.misc import MiscTestCase # noqa: F401
 
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(defaultTests())
-    # unittest.main()
+    unittest.main()
