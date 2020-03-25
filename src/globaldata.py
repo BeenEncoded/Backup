92655fff54bd11a0584b3059bd1b684a7fe2a14d
Jonathan Whitlock <beenencoded@outlook.com>
2020-03-25 14:47:05 -0400

    updated version

--- src/globaldata.py
+++ src/globaldata.py
@@ -26,7 +26,7 @@ CONFIG = Configuration()
 PDATA = ProgramData(_config=CONFIG.config)
 
 #global constants
-VERSION = semver.VersionInfo(1, 2, 3, "beta")
+VERSION = semver.VersionInfo(1, 3, 0, "beta")
 LOGS_FOLDER = os.path.abspath("./logs")
 LOGFILE = (LOGS_FOLDER + os.path.sep + "backup_program.log")
 LOG_LEVEL = {
